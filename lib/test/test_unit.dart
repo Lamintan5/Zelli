098122915d95f4a79e43dc060b1d333fcb38bd36
Lamintan5 <tech@studio5ive.org>
2024-12-22 09:30:48 +0300

    testing

--- lib/test/test_unit.dart
+++ lib/test/test_unit.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 import 'package:http/http.dart' as http;
 
+import '../api/config.dart';
+
 class MpesaPaymentScreen extends StatefulWidget {
   @override
   _MpesaPaymentScreenState createState() => _MpesaPaymentScreenState();
@@ -18,7 +20,7 @@ class _MpesaPaymentScreenState extends State<MpesaPaymentScreen> {
   }
 
   void connectToSocket() {
-    socket = IO.io('http://YOUR_SERVER_URL', <String, dynamic>{
+    socket = IO.io('http://${Config.apiURL}', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
@@ -38,11 +40,11 @@ class _MpesaPaymentScreenState extends State<MpesaPaymentScreen> {
   }
 
   Future<void> initiatePayment() async {
-    final url = Uri.parse("http://YOUR_SERVER_URL/api/pay");
+    final url = Uri.parse("http://${Config.apiURL}/api/pay");
     final response = await http.post(
       url,
       headers: {"Content-Type": "application/json"},
-      body: '{"amount": 100, "phoneNumber": "254712345678", "accountNumber": "ACC123", "businessNumber": "123456"}',
+      body: '{"amount": 100, "phoneNumber": "254113671997", "accountNumber": "ACC123", "businessNumber": "123456"}',
     );
 
     if (response.statusCode == 200) {
