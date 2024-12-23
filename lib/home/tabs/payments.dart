6992083d2c763d6e4396ecb187b6af6188513898
Lamintan5 <tech@studio5ive.org>
2024-12-23 22:44:22 +0300

    payments item

--- lib/home/tabs/payments.dart
+++ lib/home/tabs/payments.dart
@@ -1,14 +1,29 @@
 import 'dart:convert';
+import 'dart:io';
 
+import 'package:Zelli/home/actions/receipt.dart';
+import 'package:Zelli/models/data.dart';
 import 'package:Zelli/models/entities.dart';
 import 'package:Zelli/models/units.dart';
+import 'package:Zelli/utils/colors.dart';
+import 'package:Zelli/widgets/text/text_format.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:get/get.dart';
 import 'package:grouped_list/grouped_list.dart';
 import 'package:intl/intl.dart';
 
 import '../../main.dart';
+import '../../models/messages.dart';
 import '../../models/payments.dart';
+import '../../models/users.dart';
 import '../../widgets/items/item_pay.dart';
+import '../../widgets/profile_images/current_profile.dart';
+import '../../widgets/profile_images/user_profile.dart';
+import '../../widgets/shimmer_widget.dart';
+import '../actions/chat/message_screen.dart';
+import '../actions/chat/web_chat.dart';
 
 class Payments extends StatefulWidget {
   final EntityModel entity;
@@ -27,8 +42,16 @@ class Payments extends StatefulWidget {
 
 class _PaymentsState extends State<Payments> {
   List<PaymentsModel> _pay = [];
+  List<EntityModel> _enty = [];
+  List<UserModel> _user = [];
+  List<UnitModel> _unit = [];
+
+  bool _isAdmin = false;
 
   _getData(){
+    _enty = myEntity.map((jsonString) => EntityModel.fromJson(json.decode(jsonString))).toList();
+    _user = myUsers.map((jsonString) => UserModel.fromJson(json.decode(jsonString))).toList();
+    _unit = myUnits.map((jsonString) => UnitModel.fromJson(json.decode(jsonString))).toList();
     _pay = myPayment.map((jsonString) => PaymentsModel.fromJson(json.decode(jsonString))).where((test){
       bool matchesEid = widget.entity.eid.isEmpty || test.eid == widget.entity.eid.toString();
       bool matchesUnit = widget.unit.id.toString().isEmpty || test.uid == widget.unit.id.toString();
@@ -121,16 +144,11 @@ class _PaymentsState extends State<Payments> {
                       final yesterday = today.subtract(Duration(days: 1));
                       final time = DateTime.parse(payment.current.toString());
                       return Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+                        padding: const EdgeInsets.symmetric(vertical: 10),
                         child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Expanded(
-                              child: Divider(
-                                thickness: 0.5,
-                                height: 0.5,
-                                color: reverse,
-                              ),
-                            ),
                             Container(
                               padding: EdgeInsets.symmetric(horizontal: 5, vertical: 1),
                               margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
@@ -147,19 +165,129 @@ class _PaymentsState extends State<Payments> {
                                 style: TextStyle(fontSize: 10),
                               ),
                             ),
-                            Expanded(
-                              child: Divider(
-                                thickness: 0.5,
-                                height: 0.5,
-                                color: reverse,
-                              ),
-                            ),
                           ],
                         ),
                       );
                     },
                     indexedItemBuilder : (BuildContext context, PaymentsModel payment, int index) {
-                      return ItemPay(payments: payment, from: 'Home', removePay: removePay, entity: widget.entity, unit: widget.unit,);
+                      EntityModel entity;
+                      UserModel payer;
+                      UnitModel unit;
+                      var user;
+                      entity = _enty.firstWhere((element) => element.eid == payment.eid,
+                          orElse: ()=> widget.entity);
+                      payer = payment.payerid == currentUser.uid
+                          ? currentUser
+                          : _user.firstWhere((element) => element.uid == payment.payerid, orElse: () => UserModel(uid: "", image: "", username: "N/A"));
+                      unit = _unit.firstWhere((element) => element.id == payment.uid, orElse: ()=> widget.unit);
+                      user = payment.tid.toString().split(",").first == currentUser.uid
+                          ? currentUser
+                          : _user.firstWhere((element) => element.uid == payment.tid.toString().split(",").first,
+                          orElse: () => UserModel(uid: "", image: "", username: "N/A"));
+                      _isAdmin = entity.admin.toString().contains(currentUser.uid);
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 10),
+                        child: Slidable(
+                          startActionPane: payer.uid!=currentUser.uid? ActionPane(
+                            motion: ScrollMotion(),
+                            children: [
+                              SlidableAction(
+                                onPressed: (context){
+                                  Platform.isIOS || Platform.isAndroid
+                                      ? Get.to(() => MessageScreen(changeMess: _changeMess, updateCount: _updateCount, receiver: payer,), transition: Transition.rightToLeft)
+                                      : Get.to(() => WebChat(selected: payer,), transition: Transition.rightToLeft);
+                                },
+                                backgroundColor: color1,
+                                borderRadius: BorderRadius.circular(25),
+                                foregroundColor: reverse,
+                                icon: CupertinoIcons.text_bubble,
+                              ),
+                              SizedBox(width: 5,),
+                              SlidableAction(
+                                onPressed: (context){
+                                  ScaffoldMessenger.of(context).showSnackBar(
+                                    SnackBar(
+                                        content: Text("Feature currently unavailable"),
+                                        showCloseIcon: true,
+                                    )
+                                  );
+                                },
+                                backgroundColor: color1,
+                                borderRadius: BorderRadius.circular(25),
+                                foregroundColor: reverse,
+                                icon: CupertinoIcons.phone,
+                              ),
+                              SizedBox(width: 5,),
+                            ],
+                          ): null,
+                          endActionPane:  ActionPane(
+                            motion: ScrollMotion(),
+                            children: [
+                              SizedBox(width: _isAdmin? 5 : 0,),
+                              _isAdmin? SlidableAction(
+                                onPressed: null,
+                                backgroundColor: CupertinoColors.systemRed.withOpacity(0.1),
+                                borderRadius: BorderRadius.circular(25),
+                                foregroundColor: CupertinoColors.systemRed,
+                                icon: CupertinoIcons.delete,
+                              ) : SizedBox(),
+                              SizedBox(width: 5,),
+                              SlidableAction(
+                                onPressed: (context){
+                                  Get.to(() => Receipt(payment: payment), transition: Transition.rightToLeft);
+                                },
+                                backgroundColor: color1,
+                                borderRadius: BorderRadius.circular(25),
+                                foregroundColor: reverse,
+                                icon: CupertinoIcons.doc_plaintext,
+                              ),
+                            ],
+                          ),
+                          child: InkWell(
+                                  onTap: (){
+                                    Get.to(() => Receipt(payment: payment), transition: Transition.rightToLeft);
+                                  },
+                                  borderRadius: BorderRadius.circular(25),
+                                  splashColor: CupertinoColors.activeBlue,
+                                  child: Container(
+                                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
+                                    decoration: BoxDecoration(
+                                      color: color1,
+                                      borderRadius: BorderRadius.circular(25)
+                                    ),
+                                    child: Row(
+                                      children: [
+                                        payer.uid == ""
+                                            ? ShimmerWidget.circular(width: 40, height: 40)
+                                            : payer.uid==currentUser.uid? CurrentImage(radius: 20,)
+                                            :  UserProfile(image: payer.image.toString(), radius: 20,),
+                                        SizedBox(width: 15,),
+                                        Expanded(
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(payer.username.toString(), style: TextStyle(fontSize: 16)),
+                                              Text(
+                                                  '${TFormat().toCamelCase(payment.type.toString().split(',').last)} ● ${unit.title}',
+                                                  style: TextStyle(color: secondaryColor)
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                        Text(
+                                          '${payment.type!.split(",").first == "EXP"?'-':'+'}${TFormat().getCurrency()}${TFormat().formatNumberWithCommas(double.parse(payment.amount.toString()))}',
+                                          style: TextStyle(
+                                              fontSize: 15,
+                                              fontWeight: FontWeight.w700,
+                                              color: payment.type!.split(",").first != "EXP"? CupertinoColors.activeGreen : CupertinoColors.activeOrange
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                          ),
+                        ),
+                      );
                     },
                   ),
                 ),
@@ -170,6 +298,9 @@ class _PaymentsState extends State<Payments> {
       ),
     );
   }
+  _updateCount(){}
+  _changeMess(MessModel messModel){}
+
   void removePay(String payid){
     print("Removing Payment");
     _pay.removeWhere((element) => element.payid == payid);
